d3732066203c4ce67d4a438a9b792fdb872e944e
James Edmond <jae1018@wildcats.unh.edu>
2020-04-24 18:17:57 +0000

    Decided to still use OOP, but the node class added overhead just for being the middle-man so I threw it out. Using just the DNN class from before, I've restructed it to work with references to arrays and vectors (from xtensor). This commit has the initialize_network and basic class structure working properly.

--- include/DNN.h
+++ include/DNN.h
@@ -5,16 +5,17 @@
 #include "xtensor/xio.hpp"
 #include "xtensor/xview.hpp"
 #include <iostream>
-#include <vector>
+//#include <vector>
 #include <cmath>
 
 // Define vec to replace xtensor<double,1>
-using xtens = xt::xtensor<double,1>;
+using vec = xt::xtensor<double,1>;
+using arr = xt::xtensor<double,2>;
 //using all_zeros = xt::empty<double>;  <-- doesn't work for some reason???
 
 // --- Declarations ---
 // Node class
-class Node {
+/**class Node {
   private:
     double input;
     xtens weights;
@@ -25,33 +26,38 @@ class Node {
     xtens get_all_weights();
     void set_input(double new_input);
     void set_weight(int index, double new_weight);
-}; 
+};*/
 
 // DNN class
 class DNN {
   private:
-    std::vector<Node> layers[5];
-    std::vector<Node> biases;
+    const int NUM_LAYERS = 5;
+    const int LAYER_SIZES[5] = {10, 8, 6, 4, 2};
+    arr all_weights[4]; // dimens = num_layers - 1
+    vec all_biases[4];  // dimens = num_layers - 1
     // Getters & Setters
-    void set_input(int layer_num, int node_num, double new_input);
+    vec& get_biases(int layer_num);
+    arr& get_weights(int layer_num);
+    int get_num_send_nodes(arr& weights);
+    int get_num_recv_nodes(arr& weights);
+    /**void set_input(int layer_num, int node_num, double new_input);
     double get_input(int layer_num, int node_num);
     double get_weight(int layer_num, int node_num, int weight_num);
     xtens get_all_weights(int layer_num, int node_num);
-    //void set_node_weight(int layer_num, int node_num, double new_weight);
+    void set_node_weight(int layer_num, int node_num, double new_weight);
+    */
     // Non-trivial funcs
-    double activ_func(double val_in);
-    double calc_preactiv(int layer_num, int node_num);
-    void forward_propogate();
+    //double activ_func(double val_in);
+    //double calc_preactiv(int layer_num, int node_num);
+    //void forward_propogate();
+    void fill_up_weights(arr& weights);
+    void fill_up_biases(vec& biases);
     void initialize_network();
   public:
-    const int NUM_LAYERS = 5;
-    const int LAYER_SIZES[5] = {10,8,6,4,2};
-    //const int layer_sizes[5] = {800,100,50,25,10};
-    const double univ_starting_weight = .5;
     DNN();
-    void print_all_inputs();
+    //void print_all_inputs();
     void print_all_weights();
-    void compute_output();
+    //void compute_output();
 };
 
 #endif //
--- source/DNN/DNN.cxx
+++ source/DNN/DNN.cxx
@@ -1,5 +1,4 @@
 #include "DNN.h"
-#include <vector>
 
 /**
 * Note: Bias nodes are modified by setting a node_int val
@@ -30,56 +29,44 @@
 
 // ********** Private Functions **********
 
+
 // ----- Getters & Setters -----
 
-// Sets input for node at layer and node specified
-void DNN::set_input(int layer_num, int node_num, double new_input) {
-  if (node_num != -1) {
-    (  (  layers[layer_num]  ).at(node_num)  ).set_input(new_input);
-  } else {
-    (  biases.at(layer_num-1)  ).set_input(new_input);
-  }
+
+// Based on ref to weight matrix, get number of receiving nodes
+int DNN::get_num_recv_nodes(arr& weights) {
+  return (  weights.shape()  )[1];
 }
 
-// Returns input of node at layer and node specified
-double DNN::get_input(int layer_num, int node_num) {
-  if (node_num != -1) {
-    return (  (  layers[layer_num]  ).at(node_num)  ).get_input();
-  } else {
-    return (  biases.at(layer_num-1)  ).get_input(); // -1 b/c no bias in input
-  }
+
+// Based on ref to weight matrix, get number of sending nodes
+int DNN::get_num_send_nodes(arr& weights) {
+  return (  weights.shape()  )[0];
 }
 
-// Returns weight for node connection at layer, node, and connec specified.
-double DNN::get_weight(int layer_num, int node_num, int weight_num) {
-  if (node_num != -1) {
-    return (  (  layers[layer_num]  ).at(node_num)  ).get_weight(weight_num);
-  } else {
-    return (  biases.at(layer_num-1)  ).get_weight(weight_num);
-  }
+
+// Returns weights for designated layer
+// Note that input layer has no weights, so get_weights(0) throws error!
+arr& DNN::get_weights(int layer_num) {
+  return all_weights[layer_num-1];
 }
 
-// Returns *all* weights that a particular node maintains.
-xtens DNN::get_all_weights(int layer_num, int node_num) {
-  if (node_num != -1) {
-    return (  (  layers[layer_num]  ).at(node_num)  ).get_all_weights();
-  } else {
-    return (  biases.at(layer_num-1)  ).get_all_weights();
-  }
+
+// Returns biases for designated layer
+// Note that input layer has no biases, so get_biases(0) throws error!
+vec& DNN::get_biases(int layer_num) {
+  return all_biases[layer_num-1];
 }
 
-/**   * not funcitonal yet!!! *
-// Sets weight of node at layer and node specified
-void DNN::set_node_weight(int layer_num, int node_num, double new_weight) {
-  (  (  layers[layer_num]  ).at(node_num)  ).set_weight = new_weight;
-}*/
 
 // ----- Non-trivial Functions -----
 
+/**
 // The activation function for the neural network
 double DNN::activ_func(double val)  {
   return 1/(1 + exp(-val));
 }
+
 
 // Calculates the preactivation value for a node (the linear combination
 // that needs to be performed i.e. x1*w1 + x2*w2 + ...)
@@ -97,6 +84,7 @@ double DNN::calc_preactiv(int node_layer,int node_int) {
   return val;
 }
 
+
 // Forward propogates data from input layer all the way to output layer
 void DNN::forward_propogate() {
   for (int layer_num = 1; layer_num < NUM_LAYERS; layer_num++) {
@@ -105,88 +93,79 @@ void DNN::forward_propogate() {
       //set_input(layer_num, node_num, activ_func(preactiv_val));
     }
   }
-}
+}*/
 
-// Starts nodes out from scratch (no prior weights saved to file imported here!!)
-void DNN::initialize_network() {
-  // For each layer ...
-  for (int i = 0; i < NUM_LAYERS; i++) {
-    // Make a node ...
-    for (int num_node = 0; num_node < LAYER_SIZES[i]; num_node++)  {
-      // With weights for each connection to node in next layer ...
-      xtens weights;
-      if (i > 0) {  // no weights needed for input layer
-	weights = xt::empty<double>({LAYER_SIZES[i-1]});
-        weights.fill(univ_starting_weight);  // try RNG next time
-      }
-      // Then create the node and add it to the vector
-      Node new_node = Node(0.5, weights);
-      (layers[i]).push_back(new_node);
-    }
-    // and don't forget to make a bias node for each non-output layer!
-    if ( i > 0) {
-      xtens weights = xt::empty<double>({LAYER_SIZES[i]});
-      weights.fill(univ_starting_weight);
-      Node bias = Node(0.5,weights);
-      biases.push_back(bias);
+
+// Fill up all entries in weights arr
+void DNN::fill_up_weights(arr& weights) {
+  int num_recv = get_num_recv_nodes(weights);
+  int num_send = get_num_send_nodes(weights);
+  for (int recv = 0; recv < num_recv; recv++) {
+    for (int send = 0; send < num_send; send++) {
+      weights(send,recv) = double(recv*10 + send)/100;
     }
   }
 }
 
+
+// Fill up all entires in biases vec
+void DNN::fill_up_biases(vec& biases) {
+  for (int i = 0; i < biases.size(); i++) {
+    biases(i) = double(i)/100;
+  }
+}
+
+
+// Starts nodes out from scratch (no prior weights saved to file imported here!!)
+void DNN::initialize_network() {
+  for (int i = 0; i < NUM_LAYERS-1; i++) {
+    // Make weights matrix between adjacent layers
+    arr weights = xt::empty<double>({LAYER_SIZES[i],LAYER_SIZES[i+1]});
+    fill_up_weights(weights);
+    all_weights[i] = weights;
+    // Make biases for non-input layers
+    vec biases = xt::empty<double>({LAYER_SIZES[i+1]});
+    fill_up_biases(biases);
+    all_biases[i] = biases;
+  }
+}
+
+
 // ----- Public Constructor -----
+
 
 DNN::DNN() {
   initialize_network();
 }
 
+
 // ----- Public Functions -----
 
-// Prints the input for all nodes
-void DNN::print_all_inputs() {
-  std::cout << "----- printing inputs -----\n";
-  for (int i = 0; i < NUM_LAYERS; i++) {
-    std::cout << "*** Layer " << i + 1 << " ***\n";
-    std::cout << "[ ";
-    for (int node_num = 0; node_num < LAYER_SIZES[i]; node_num++) {
-      std::cout << get_input(i,node_num) << " ";
-    }
-    std::cout << "]";
-    if (i > 0) {
-      std::cout << " || Bias node input [ " << get_input(i,-1) << " ]";
-    }
-    std::cout << "\n\n";
-  }
-}
 
 // Prints all the weights for all nodes
 void DNN::print_all_weights() {
-  std::cout << "----- printing weights -----\n";
-  for (int i = 0; i < NUM_LAYERS; i++) {
-    std::cout << "*** Layer " << i + 1 << " ***\n";
-    for (int node_num = 0; node_num < LAYER_SIZES[i]; node_num++) {
-      std::cout << "Node " << node_num + 1 << " has the following weights:\n";
-      xtens weights = get_all_weights(i,node_num);
-      std::cout << "[ ";
-      for (int weight_num = 0; weight_num < weights.size(); weight_num++) {
-        std::cout << weights(weight_num) << " ";
+  std::cout << "----- printing weights -----\n"
+            << "*** Layer 1 is input layer (so no weights or biases) ***\n";
+  for (int i = 1; i < NUM_LAYERS; i++) {
+    std::cout << "\n*** Layer " << i + 1 << " ***\n";
+    arr weights = get_weights(i);
+    vec biases = get_biases(i);
+    int num_recv = get_num_recv_nodes(weights);
+    int num_send = get_num_send_nodes(weights);
+    for (int r_node = 0; r_node < num_recv; r_node++) { // r = receiving
+      std::cout << "Node " << r_node + 1 << " has the following weights:\n[ ";
+      for (int s_node = 0; s_node < num_send; s_node++) { // s = sending
+        std::cout << weights(s_node,r_node) << " ";
       }
-      std::cout << "]\n";
-    }
-    if (i > 0) {
-      std::cout << "Bias node has the following weights:\n";
-      xtens weights = get_all_weights(i,-1);
-      std::cout << "[ ";
-      for (int a = 0; a < weights.size(); a++) {
-        std::cout << weights(a) << " ";
-      }
-      std::cout << "]\n\n";
+      std::cout << "] with bias = " << biases(r_node) << "\n";
     }
   }
 }
 
+
 // Test of forward propogate
-void DNN::compute_output() {
+/**void DNN::compute_output() {
   std::cout << "----- forward propagating -----\n";
   forward_propogate();
   std::cout << "Output is " << get_input(NUM_LAYERS - 1, 0) << "\n";
-}
+}*/
